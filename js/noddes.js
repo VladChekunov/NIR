7aa7ea22f380299954924f1cf584e792c503da1b
Vlad Chekunov <vladlenchekunov@yandex.com>
2019-03-15 08:43:18 +0400

    touchpad support

--- js/noddes.js
+++ js/noddes.js
@@ -5,7 +5,20 @@ var noddes = {
 	},
 	initEvents:function(){
 		noddes.initKeyboard();
-		document.addEventListener('mousedown', function(e){
+		//Mouse
+		document.addEventListener('mousedown', noddes.events.startmove);
+		document.addEventListener('mousemove', noddes.events.move);
+		document.addEventListener('mouseup', noddes.events.endmove);
+		//Touchpad
+		document.addEventListener('touchstart', noddes.events.move);
+		document.addEventListener('touchmove', noddes.events.move);
+		document.addEventListener('touchend', noddes.events.move);/*function(e){
+			
+		});*/
+		
+	},
+	events:{
+		startmove:function(e){
 			console.log('down');
 			
 			if(noddes.states.keyboardState.spkey){
@@ -15,37 +28,33 @@ var noddes = {
 					x:e.clientX,
 					y:e.clientY
 				};
-				noddes.states.nodesViewport={
-					x:parseInt(document.getElementById("NodesPanel").children[0].style.marginLeft),
-					y:parseInt(document.getElementById("NodesPanel").children[0].style.marginTop)
-				}
+				noddes.states.nodesViewport.x=parseInt(document.getElementById("NodesPanel").children[0].style.marginLeft)
+				noddes.states.nodesViewport.y=parseInt(document.getElementById("NodesPanel").children[0].style.marginTop)
 				noddes.changeCursor("move");
 			}
-			
-		});
-		document.addEventListener('mouseup', function(e){
+		},
+		move:function(e){
+			if(noddes.states.actionState.move){
+				newx = noddes.states.nodesViewport.x+(-1)*(noddes.states.actionState.moveStart.x-e.clientX);
+				newy = noddes.states.nodesViewport.y+(-1)*(noddes.states.actionState.moveStart.y-e.clientY);
+				//console.log("Мы двигаем на "+newx+" и "+newy);
+				//Учесть Scale
+
+				if(newx*noddes.states.nodesViewport.z < 1500 && newx*noddes.states.nodesViewport.z > -1500 && newy*noddes.states.nodesViewport.z < 1500 && newy*noddes.states.nodesViewport.z > -1500){
+					document.getElementById("NodesPanel").children[0].style.marginLeft=newx+"px";
+					document.getElementById("NodesPanel").children[0].style.marginTop=newy+"px";
+				}
+				//
+			}
+			//console.log('move');
+		},
+		endmove:function(e){
 			console.log('up');
 			if(noddes.states.keyboardState.spkey){
 				noddes.states.actionState.move=false;
 				noddes.changeCursor("grab");
 			}
-		});
-		document.addEventListener('mousemove', function(e){
-			
-			if(noddes.states.actionState.move){
-				//console.log(e);
-				//
-				newx = noddes.states.actionState.moveStart.x-e.clientX;
-				newy = noddes.states.actionState.moveStart.y-e.clientY;
-				//console.log("Мы двигаем на "+newx+" и "+newy);
-				document.getElementById("NodesPanel").children[0].style.marginLeft=noddes.states.nodesViewport.x+(-1)*newx+"px";
-				document.getElementById("NodesPanel").children[0].style.marginTop=noddes.states.nodesViewport.y+(-1)*newy+"px";
-				
-				//
-			}
-			//console.log('move');
-		});
-		
+		}
 	},
 	initKeyboard:function(){
 		document.addEventListener('keydown', function(e){
@@ -93,6 +102,21 @@ var noddes = {
 		}
 		console.log("Курсор: "+cursorType)
 	},
+	data:[
+		{
+			type:"text",
+			color:"#fff000",
+			x:335,
+			y:169,
+			//...
+			data:{
+				text:"Hello World",
+				font:"Arial",
+				//...
+				size:"20"
+			}
+		}
+	],
 	states:{
 		keyboardState:{
 			ctkey:false,
@@ -109,7 +133,8 @@ var noddes = {
 		},
 		nodesViewport:{
 			x:0,
-			y:0
+			y:0,
+			z:1//Zoom
 		}
 	}
 }
